925a5d34ccbe76f0742bbe13d6b76eeb2fd7e7a2
MarynaChi <50667666+MarynaChi@users.noreply.github.com>
2019-05-20 22:06:15 +0300

    add first task

---
+++ Task01/Car.java
@@ -0,0 +1,11 @@
+public class Car{
+   public static void main(String[] args){
+      System.out.println("JAVA is great!!!");
+   }
+}
+class Prob
+{
+    void getCar(){
+      System.out.println("Car!");
+    }
+}
